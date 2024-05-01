8e5942b0c652927edbc559670dc466f03acbc07d
Francisco <fserra62@stu.jjc.edu>
2024-05-01 13:12:07 -0500

    Character creation/movement, wall borders

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
+let player;
+
+let borderLeft;
+
+let borderRight;
+
+let score = 0;
+
 function setup() {
-    createCanvas(400, 400);
+  
+  new Canvas(500, 500);
+  
+  borderLeft = new Sprite(0,450, 0, 20, 'static');
+  
+  borderRight = new Sprite(500,450, 0, 20, 'static');
+  
+  player = new Sprite();
+  
+  player.x = 255;
+  
+  player.y = 450;
+  
 }
 
 function draw() {
+    background('black');
+     
+    textSize(25);
+  
+    fill("purple");
+  
+    text('Score: '+ score,10,30);
+  
+    player.speed = 15;
+  
+  if (kb.pressing('left')){
+    player.direction = 180;
     
+  } else if (kb.pressing('right')){
+    player.direction = 0
+    
+  } else {
+    player.speed = 0;
+    
+  }
+  if (player.collides(borderLeft)){
+    player.speed = 0;
+  }
+  if (player.collides(borderRight)){
+    player.speed = 0;
+  }
+      
+    
+  
+  
+  
+  // if(player.overlaps(border1){
+  //    player.speed = 0;
+  //    }
+  
+  
+  
 }
+
+    
