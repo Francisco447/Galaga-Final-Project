5a5e158e3e35febd2ee4e67caa9dadf89202ad45
Francisco <fserra62@stu.jjc.edu>
2024-05-06 20:48:14 -0500

    bottom border added to allow for losing of points game win/lose functions added

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ let borderLeft;
 
 let borderRight;
 
+let borderBottom;
+
 let score = 0;
 
 let bullets;
 
 let enemies;
-
 
 function setup() {
   
@@ -18,6 +19,8 @@ function setup() {
   borderLeft = new Sprite(0,450, 0, 200, 'static');
   
   borderRight = new Sprite(500,450, 0, 200, 'static');
+  
+  borderBottom = new Sprite(200,500,499,10,'')
   
   player = new Sprite(255,450, 45, 20, '')
   
@@ -35,15 +38,11 @@ function setup() {
   
   enemies.overlaps(player, endGame);
   
+  enemies.overlaps(borderBottom, losePoint);
+  
   bullets.overlaps(enemies, scorePoint);
   
-  enemies.x = 200;
-  
-  enemies.y = 200;
-  
   enemies.diameter = 20;
-  
-  // enemies.y -= 5;
   
   enemies.color = 'red';
   
@@ -51,7 +50,7 @@ function setup() {
 
 function draw() {
     background('black');
-     
+  
     textSize(25);
   
     fill("purple");
@@ -63,67 +62,90 @@ function draw() {
   if (kb.pressing('left')){
     player.direction = 180;
     
-  } else if (kb.pressing('right')){
+  } 
+  else if (kb.pressing('right')){
     player.direction = 0
     
-  } else {
+  } 
+  else {
     player.speed = 0;
     
-  }
+  } 
+  
       
   if (kb.presses('space')){
     
-  let firedBullet = new bullets.Sprite(player.x,player.y);
+  let firedBullet = new     bullets.Sprite(player.x,player.y);
   
   firedBullet.direction = 270;
  
   firedBullet.speed = 20; 
     
-  // bullets.life = 5;
   }
   
   if (kb.presses('space')){
+    
   let newEnemy = new enemies.Sprite()
   
     newEnemy.x = random(15,485)
     
-    newEnemy.y = 0
+    newEnemy.y = 0;
     
     newEnemy.direction = -270;
     
     newEnemy.speed = 2;
-  
     
   }
   
- 
-  
+  if (score >= 15){
+    gameWin();
+  }
   
 }
 
+
 function endGame(){
+  text('Click to Restart', 170,160);
+
   noLoop();
+  
 }
 
 function scorePoint(bullets, enemies){
+  
   score++
+  
   enemies.remove();
+  
   bullets.remove();
+  
 }
 
+function losePoint(enemies){
+  score -= 2
+  enemies.remove();
+}
 
 function mousePressed(){
   
-//   if (isLooping(false, mousePressed)){
-    
-//   }
-  
   enemies.removeAll();
+  
   bullets.removeAll();
+  
   loop();
+  
   score = 0;
+  
   player.x = 255;
+  
   player.y = 450;
+  
+}
+
+function gameWin(){
+  text('\t\t\t\t\tYou Win! \n Double Click to restart', 130, 160);
+  noLoop();
+  
 }
 
 
